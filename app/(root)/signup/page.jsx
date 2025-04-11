e2c939e9c39da1941987184379958379623a2ef2
Purnika30 <purnikasingh2005@gmail.com>
2025-04-12 03:42:34 +0530

    update sign up

--- app/(root)/signup/page.jsx
+++ app/(root)/signup/page.jsx
@@ -29,31 +29,35 @@ export default function SignUp() {
       console.error('Error:', err.message);
       alert(err.message);
     }
-    // Send to OTP page with email in query
-    // router.push(`/verify-otp?email=${encodeURIComponent(email)}`);
   };
 
-
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <form onSubmit={handleSubmit} className="bg-white p-6 rounded-2xl shadow-md w-full max-w-md">
-        <h2 className="text-2xl font-bold mb-4 text-center">Sign Up</h2>
+    <div
+      className="flex items-center justify-center min-h-screen bg-gray-100"
+      style={{
+        backgroundImage: `url('https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&auto=format&fit=crop&w=1350&q=80')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
+      <form onSubmit={handleSubmit} className="bg-[#123458] p-6 rounded-2xl shadow-md w-full max-w-md">
+        <h2 className="text-2xl font-bold mb-4 text-center text-white">Sign Up</h2>
         
-        <label className="block mb-2 text-sm font-medium text-gray-700">
+        <label className="block mb-2 text-sm font-medium text-white">
           Email Address
         </label>
         <input
           type="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          className="w-full p-2 mb-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="w-full p-2 mb-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder:text-white"
           placeholder="you@example.com"
           required
         />
 
         <button
           type="submit"
-          className="w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 transition"
+          className="w-full bg-[#D4C9BE] text-black py-2 rounded-md hover:bg-[#c0b5a9] transition"
         >
           Proceed to Send OTP
         </button>
